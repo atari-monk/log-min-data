3c81866963d3826490e804b17b32aa27ad1a634e
krzm <kmazanek@gmail.com>
2022-09-01 18:06:33 +0200

    fix context

--- Log.Min.Data/Context/LogDbContext.cs
+++ Log.Min.Data/Context/LogDbContext.cs
@@ -4,30 +4,14 @@ using Microsoft.EntityFrameworkCore;
 namespace Log.Min.Data;
 
 public class LogDbContext
-    : DbContextExtended
+    : DbContextDbConfig
 {
     public DbSet<LogModel>? Log { get; set; }
     public DbSet<Task>? Task { get; set; }
     public DbSet<Category>? Category { get; set; }
     public DbSet<Place>? Place { get; set; }
 
-    //todo: make this virtual
     protected override void SeedData(ModelBuilder builder)
     {
-    }
-
-    private object GetEntity(
-        int id
-        , string name
-        , string? description = default)
-    {
-        return new 
-        { 
-            Id = id
-            , Name = name
-            , Description = description
-            , CreatedDate = DateTime.Now
-            , UpdatedDate = DateTime.Now
-        };
     }
 }
